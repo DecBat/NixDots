36c732fc86d2d1a944e75d11ee0e35f24505ed31
Declan Battell <declan@battell.com>
2026-08-03 16:59:05 -0400

    introduced new packages for hyprland

--- configuration.nix
+++ configuration.nix
@@ -19,9 +19,36 @@
 
   
 
-  environment.systemPackages = with pkgs; [ git neovim curl ];
+  environment.systemPackages = with pkgs; [ 
+    git
+    neovim
+    curl
+    kitty
+    waybar
+    rofi
+    mako
+    libnotify
+    wl-clipboard
+    cliphist
+    grim
+    slurp
+    brightnessctl
+    playerctl
+    pavucontrol
+    hyprpaper
+    hyprlock
+    hypridle
+    hyprpolkitagent ];
+
+
   services.openssh.enable = true;
   programs.ssh.startAgent = true;
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts
+    noto-fonts-emoji
+  ];
 
   system.stateVersion = "26.05";
 }
